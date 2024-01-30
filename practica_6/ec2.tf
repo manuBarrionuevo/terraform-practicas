8b93ee4795477884e1ac72bf67786481474156b4
manuelBarrionuevo <manuelbarrionuevo678@hotmail.com>
2024-01-30 15:41:07 -0300

    update

--- practica_6/ec2.tf
+++ practica_6/ec2.tf
@@ -1,16 +1,38 @@
 
 resource "aws_instance" "public_instance" {
-  ami                     = "ami-0005e0cfe09cc9050"
-  instance_type           = "t2.micro"
-  subnet_id  = aws_subnet.public_subnet.id 
-  key_name = data.aws_key_pair.key.key_name
-   tags = {
+  ami                    = "ami-0005e0cfe09cc9050"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_subnet.id
+  key_name               = data.aws_key_pair.key.key_name
+  vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
+  user_data              = <<EOF
+#!/bin/bash
+echo "esto es un mensaje" > ~/mensaje.txt
+EOF
+  tags = {
     "Name" = "App-Desa"
   }
-
-  vpc_security_group_ids = [ aws_security_group.sg_public_instance.id ]
   //lifecycle {
-   // prevent_destroy = true
+  // prevent_destroy = true
   //}
+  provisioner "local-exec" {
+    command = "echo instancia creada con IP ${aws_instance.public_instance.public_ip} >> datos_instancia.txt"
+  }
+  provisioner "local-exec" {
+    when    = destroy
+    command = "echo instancia destruida con IP ${self.public_ip} >> datos_instancia.txt"
+  }
+  /*   provisioner "remote-exec" {
+    inline = [
+      "echo 'Hola Mundo' > ~/saludo.txt"
+    ]
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ec2-user"
+      private_key = file("test-k.pem")
+    }
+  } */
+
 }
 
--- practica_6/variables.tf
+++ practica_6/variables.tf
@@ -27,3 +27,8 @@ variable "sg_ingress_cidr" {
   description = "CIDR for ingress traffic"
   type = string
 }
+
+variable "ec2_specs" {
+  description = "Parametros de la instancia"
+  type = map(string)
+}
