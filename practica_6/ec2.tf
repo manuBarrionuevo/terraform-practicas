36bb7ca87627352fd52a62e085a5ac2c490645c6
manuelBarrionuevo <manuelbarrionuevo678@hotmail.com>
2024-01-30 15:47:26 -0300

    upgrade

--- practica_6/ec2.tf
+++ practica_6/ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "public_instance" {
   subnet_id              = aws_subnet.public_subnet.id
   key_name               = data.aws_key_pair.key.key_name
   vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
-  user_data              = file("userdata.sh")
+  user_data              = file("script/userdata.sh")
   tags = {
     "Name" = "App-Desa"
   }
