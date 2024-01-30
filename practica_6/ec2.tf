2f0f734a2184856c9f19744f66751973b162dd66
manuelBarrionuevo <manuelbarrionuevo678@hotmail.com>
2024-01-30 12:10:41 -0300

    lifecycle

--- practica_6/ec2.tf
+++ practica_6/ec2.tf
@@ -2,10 +2,13 @@
 resource "aws_instance" "public_instance" {
   ami           = "ami-0005e0cfe09cc9050"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_subnet.id
+  subnet_id     = aws_subnet.private_subnet
   key_name      = data.aws_key_pair.key.key_name
   tags = {
     "Name" = "App-Desa"
+  }
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
