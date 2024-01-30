cc26d55a548c1740e6cb0308c4b6f59c2d25a2eb
manuelBarrionuevo <manuelbarrionuevo678@hotmail.com>
2024-01-30 15:43:25 -0300

    variable ec2_specs

--- practica_6/ec2.tf
+++ practica_6/ec2.tf
@@ -1,7 +1,7 @@
 
 resource "aws_instance" "public_instance" {
-  ami                    = "ami-0005e0cfe09cc9050"
-  instance_type          = "t2.micro"
+  ami                    = var.ec2_specs.ami
+  instance_type          = var.ec2_specs.instance_type
   subnet_id              = aws_subnet.public_subnet.id
   key_name               = data.aws_key_pair.key.key_name
   vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
