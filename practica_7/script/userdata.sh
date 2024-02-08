a9fe622c88d977ef4b4d65b7d50e1d15a5c333ea
manuelBarrionuevo <manuelbarrionuevo678@hotmail.com>
2024-02-08 12:23:09 -0300

    update

--- practica_7/script/userdata.sh
+++ practica_7/script/userdata.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 echo "esto es un mensaje" > ~/mensaje.txt
 yum update -y
 yum install httpd -y
-systemclt enable httpd
-systectl start httpd
+yum install htop -y
+systemctl enable httpd
+systemctl start httpd
