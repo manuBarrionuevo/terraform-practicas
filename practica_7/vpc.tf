5288eb9d9f164699e6094cedc72406a534fd64fd
grupo7devops <manuelbarrionuevo678@hotmail.com>
2024-02-20 16:06:44 -0300

    update

--- practica_7/vpc.tf
+++ practica_7/vpc.tf
@@ -68,6 +68,7 @@ resource "aws_security_group" "sg_public_instance" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
+#tfsec:ignore:aws-ec2-no-public-egress-sgr
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
